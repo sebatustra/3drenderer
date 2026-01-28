eac459549d885005c13250e9b4e6954dea0b8dde
sebatustra <sebatustra@protonmail.com>
2026-01-28 11:27:30 -0300

    rendering grid

--- src/main.c
+++ src/main.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
+#include <stdint.h>
 #include <stdbool.h>
 #include <SDL2/SDL.h>
 
 bool is_running = false;
+
 SDL_Window* window = NULL;
 SDL_Renderer* renderer = NULL;
+
+uint32_t* color_buffer = NULL;
+SDL_Texture* color_buffer_texture = NULL;
+
+int window_width = 800;
+int window_height = 600;
 
 bool initialize_window(void) {
     if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
@@ -12,13 +20,20 @@ bool initialize_window(void) {
         return false;
     }
 
+    // use SDL to query the full screen of the monitor
+    SDL_DisplayMode display_mode;
+    SDL_GetCurrentDisplayMode(0, &display_mode);
+
+    window_width = display_mode.w;
+    window_height = display_mode.h;
+
     // Create a SDL window
     window = SDL_CreateWindow(
         NULL,
         SDL_WINDOWPOS_CENTERED,
         SDL_WINDOWPOS_CENTERED,
-        800,
-        600,
+        window_width,
+        window_height,
         SDL_WINDOW_BORDERLESS
     );
     if (!window) {
@@ -27,17 +42,29 @@ bool initialize_window(void) {
     }
 
     // Create a SDL renderer
-    renderer = SDL_CreateRenderer(window,-1,0);
+    renderer = SDL_CreateRenderer(window, -1, 0);
     if (!renderer) {
         fprintf(stderr, "Error creating SDL renderer.\n");
         return false;
     }
 
+    SDL_SetWindowFullscreen(window, SDL_WINDOW_FULLSCREEN);
+
     return true;
 }
 
 void setup(void) {
-    // TODO:
+    // allocate required memory in bytes to hold the color buffer
+    color_buffer = (uint32_t*) malloc(sizeof(uint32_t) * window_width * window_height);
+
+    // creating an SDL texture that is used to display the color buffer
+    color_buffer_texture = SDL_CreateTexture(
+        renderer,
+        SDL_PIXELFORMAT_ARGB8888,
+        SDL_TEXTUREACCESS_STREAMING,
+        window_width,
+        window_height
+    );
 }
 
 void process_input(void) {
@@ -49,10 +76,9 @@ void process_input(void) {
             is_running = false;
             break;
         case SDL_KEYDOWN:
-            if (event.key.keysym.sym == SDLK_ESCAPE) {
+            if (event.key.keysym.sym == SDLK_ESCAPE)
                 is_running = false;
-                break;
-            }
+            break;
     }
 }
 
@@ -60,14 +86,62 @@ void update(void) {
     // TODO:
 }
 
+void clear_color_buffer(uint32_t color) {
+    for (int y = 0; y < window_height; y++) {
+        for (int x = 0; x < window_width; x++) {
+            color_buffer[(window_width * y) + x] = color;
+        }
+    }
+}
+
+void draw_grid(void) {
+    // TODO:
+    // draw a background grid that fill the entire window
+    // lines should be rendered at every row-col multiple of 100
+
+    for (int y = 0; y < window_height; y += 10) {
+        for (int x = 0; x < window_width; x += 10) {
+            color_buffer[(window_width * y) + x] = 0xFF333333;
+        }
+    }
+}
+
+void render_color_buffer(void) {
+    SDL_UpdateTexture(
+        color_buffer_texture,
+        NULL,
+        color_buffer,
+        (int)(window_width * sizeof(uint32_t))
+    );
+
+    SDL_RenderCopy(
+        renderer,
+        color_buffer_texture,
+        NULL,
+        NULL
+    );
+}
+
 void render(void) {
-    SDL_SetRenderDrawColor(renderer,255, 0, 0, 255);
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
     SDL_RenderClear(renderer);
+
+    draw_grid();
+
+    render_color_buffer();
+    clear_color_buffer(0xFF000000);
 
     SDL_RenderPresent(renderer);
 }
 
-int main(void) {    
+void destroy_window(void) {
+    free(color_buffer);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+}
+
+int main(void) {
     is_running = initialize_window();    
 
     setup();
@@ -77,6 +151,8 @@ int main(void) {
         update();
         render();
     }
+
+    destroy_window();
 
     return 0;
 }
